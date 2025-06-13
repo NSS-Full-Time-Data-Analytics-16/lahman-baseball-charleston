319d8e4700081383dea2b80450be7ea4f9ef5e79
Cory Yeomans <yeocory@gmail.com>
2025-06-13 13:58:10 -0500

    final push

--- cory_yeomans_lahman_project.sql
+++ cory_yeomans_lahman_project.sql
@@ -2,8 +2,8 @@
 
 -- Question 1 
 
-SELECT MIN(year), MAX(year)
-FROM homegames;
+SELECT MIN(yearid), MAX(yearid)
+FROM teams;
 -- 1871 to 2016
 
 
@@ -84,13 +84,14 @@ SELECT CASE WHEN yearid BETWEEN 1920 AND 1929 THEN '1920s'
 			WHEN yearid BETWEEN 1990 AND 1999 THEN '1990s'
 			WHEN yearid BETWEEN 2000 AND 2009 THEN '2000s'
 			WHEN yearid BETWEEN 2010 AND 2020 THEN '2010s' END AS decade,
-			ROUND(SUM(so)::numeric / SUM(g), 2) AS avg_so_per_game,
-			ROUND(SUM(hr)::numeric / SUM(g), 2) AS avg_hr_per_game
+			ROUND(SUM(so)::numeric / (SUM(g)/2), 2) AS avg_so_per_game,
+			ROUND(SUM(hr)::numeric / (SUM(g)/2), 2) AS avg_hr_per_game
 
 
-FROM batting
+FROM teams
 WHERE yearid >= 1920
-GROUP BY decade;
+GROUP BY decade
+ORDER BY decade;
 -- Strikeouts increased greatly over time where homeruns have barely increased
 
 
@@ -140,48 +141,69 @@ WHERE w = highest_wins AND wswin = 'Y'
 -- 12 Teams Won with having the most wins
 
 WITH max_wins AS (
-	SELECT yearid, MAX(w) AS highest_wins, COUNT(*)
-	FROM teams
-	WHERE yearid BETWEEN 1970 AND 2016
-	GROUP BY yearid
-)
-SELECT name, yearid
-FROM teams
-JOIN max_wins AS mw USING(yearid)
-WHERE w = highest_wins AND wswin = 'Y'
+		SELECT yearid, MAX(w) AS highest_wins, COUNT(*)
+		FROM teams
+		WHERE yearid BETWEEN 1970 AND 2016
+		GROUP BY yearid
+	),
+	max_winning_teams AS (
+		SELECT name, yearid
+		FROM teams
+		JOIN max_wins AS mw USING(yearid)
+		WHERE w = highest_wins AND wswin = 'Y'
+	)
+SELECT COUNT(*) AS teams_total, COUNT(mwt.name) AS teams_with_max_wins, 
+		(COUNT(mwt.name)::numeric)/(COUNT(*)::numeric) AS percentage_of_ws_max_wins
+FROM max_wins
+LEFT JOIN max_winning_teams AS mwt USING(yearid);
+-- About 25.5% 
+
+
 
 -- Question 8 
+
+SELECT t.name, park_name, (SUM(hg.attendance::numeric)/SUM(hg.games::numeric)) AS avg_attendance_per_game
+FROM homegames AS hg
+JOIN parks AS p USING(park)
+JOIN teams AS t ON hg.team = t.teamid
+WHERE hg.year = 2016 AND hg.games >= 10 AND t.yearid = 2016
+GROUP BY t.name, park_name
+ORDER BY avg_attendance_per_game DESC;
+
+SELECT t.name, park_name, (SUM(hg.attendance::numeric)/SUM(hg.games::numeric)) AS avg_attendance_per_game
+FROM homegames AS hg
+JOIN parks AS p USING(park)
+JOIN teams AS t ON hg.team = t.teamid
+WHERE hg.year = 2016 AND hg.games >= 10 AND t.yearid = 2016
+GROUP BY t.name, park_name
+ORDER BY avg_attendance_per_game;
 
 
 
 -- Question 9 
 
-WITH tsn_winners AS (
-    SELECT 
-        am.playerid,
-        p.namefirst, p.namelast,
-        am.yearid,
-        am.lgid,
-        m.teamid
-    FROM awardsmanagers AS am
-    JOIN people p ON am.playerid = p.playerid
-    JOIN managers m ON am.playerid = m.playerid AND am.yearid = m.yearid
-    WHERE am.awardid = 'TSN Manager of the Year'
-),
-dual_league_winners AS (
-    SELECT playerid
-    FROM tsn_winners
-    GROUP BY playerid
-    HAVING COUNT(DISTINCT lgid) >= 2
+WITH tsn_winners_ids AS (
+		(SELECT playerid
+		FROM awardsmanagers
+		WHERE awardid = 'TSN Manager of the Year' AND lgid = 'NL')
+	INTERSECT
+		(SELECT playerid
+		FROM awardsmanagers
+		WHERE awardid = 'TSN Manager of the Year' AND lgid = 'AL')
+		),
+	manager_id_and_year AS (
+		SELECT playerid, yearid, lgid
+		FROM awardsmanagers
+		JOIN tsn_winners_ids USING(playerid)
 )
-SELECT 
-    t.namefirst, t.namelast,
-    t.lgid,
-    t.teamid,
-    t.yearid
-FROM tsn_winners AS t
-JOIN dual_league_winners d ON t.playerid = d.playerid
-ORDER BY t.namefirst, t.namelast, t.yearid;
+SELECT myi.playerid, myi.yearid, myi.lgid, namefirst, namelast, teamid, t.name
+FROM manager_id_and_year AS myi
+LEFT JOIN people USING(playerid)
+JOIN managers AS m USING(playerid)
+JOIN teams AS t USING(teamid)
+WHERE myi.yearid = m.yearid AND myi.yearid = t.yearid
+ORDER BY myi.lgid;
+-- 2 people, Jim Leyland and Davey Johnson
 
 
 
@@ -208,17 +230,42 @@ SELECT
   h.hr AS hr_in_2016
 FROM hr_2016 AS h
 JOIN career_lengths c ON h.playerid = c.playerid
-JOIN career_highs ch ON h.playerid = ch.playerid AND h.hr = ch.max_hr
+JOIN career_highs ch ON h.playerid = ch.playerid
 JOIN people p ON h.playerid = p.playerid
-ORDER BY h.hr DESC;
+WHERE h.hr = ch.max_hr;
 
 
 
+-- Question 11 
+
+SELECT t.yearid, t.name, SUM(w) AS total_wins, (AVG(salary))
+FROM teams AS t
+JOIN salaries AS s USING(teamid)
+WHERE t.yearid >= 2000
+GROUP BY t.yearid, t.name
+ORDER BY t.yearid, total_wins DESC;
+-- There is a slight pattern of higher salaries going to more winning teams and vice versa 
+-- but mostly there are a lot of outliers such as big names in the sport having high salaries regradless 
+-- of wins such as New York Yankees and Boston Red Sox and there was a pattern of a very avg salary team 
+-- (Oakland Athletics) having a high amount of wins even though their salaries were consistently low for a large span of time
 
 
 
+-- Question 12 
 
 
+-- Part A
+SELECT t.name, park_name, (SUM(hg.attendance::numeric)/SUM(hg.games::numeric)) AS avg_attendance_per_game, SUM(t.w)
+FROM homegames AS hg
+JOIN parks AS p USING(park)
+JOIN teams AS t ON hg.team = t.teamid
+WHERE hg.year = 2012 AND hg.games >= 10 AND t.yearid = 2012
+GROUP BY t.name, park_name
+ORDER BY avg_attendance_per_game DESC;
+-- There is a little evidence that could suggest the attendance correlates with the wins as it seems the top 50% of highest 
+-- attendings gets wins in the low 80s to high 90s with one team even peaking 100 whereas then bottom 50% has mostly ranges 
+-- in the high 60s to mid 80s although it is impportant this seems to fluxuate greatly throughout the years, especially in 
+-- 2012 and 2013 where the pattern doesn't appear to be as true.
 
 
 
